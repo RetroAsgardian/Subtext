c4e598fd80194d90e3c651247a7e05ab268f517d
RetroAsgardian <oskarsmacaw@gmail.com>
2020-01-06 13:50:57 -0500

    Add migration

---
+++ Migrations/20200106184858_BoardNamesNotUnique.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Subtext.Migrations
+{
+    public partial class BoardNamesNotUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Boards_Name",
+                table: "Boards");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boards_Name",
+                table: "Boards",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Boards_Name",
+                table: "Boards");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boards_Name",
+                table: "Boards",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+        }
+    }
+}
